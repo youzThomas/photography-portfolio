90ff34dc32fd9ec48c59442293aa8fe674feac83
Thomas You <144377617+youzThomas@users.noreply.github.com>
2026-03-03 23:15:27 -0500

    add pic

--- lib/images.ts
+++ lib/images.ts
@@ -7,15 +7,23 @@ import photo6 from '../public/images/pic6.jpg';
 import photo7 from '../public/images/pic7.jpg';
 import photo8 from '../public/images/pic8.jpg';
 import photo9 from '../public/images/pic9.jpg';
+import photo10 from '../public/images/pic10.jpg';
+import photo11 from '../public/images/pic11.jpg';
+import photo12 from '../public/images/pic12.jpg';
+import photo13 from '../public/images/pic13.jpg';
 
 export default [
   photo1,
   photo2,
-  photo3,
+  photo13,
   photo4,
   photo5,
   photo6,
   photo7,
   photo8,
   photo9,
+  photo10,
+  photo11,
+  photo12,
+  photo3,
 ];
